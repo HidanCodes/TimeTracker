6bf05cdf890dbb0c27752239c3f2a0bdcfa99313
Arman <armaneheme@gmail.com>
2025-02-23 10:38:04 +0330

    make urls file for projects

--- urls.py
+++ urls.py
@@ -1,23 +1,16 @@
-"""
-URL configuration for TimeTracker project.
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-     path('Timelist/',include('Time.urls'))
-]
+from .views import UserView,ProjectView,TimeEntiryView,UserDetail,ProjectsDetail,TimeEntiryDetail
+
+
+
+
+urlpatterns = [ 
+    path('userview/',UserView.as_view()),
+    path('projectview/',ProjectView.as_view()),
+    path('TimeEntiryView/',TimeEntiryView.as_view()),
+    path('userview/<int:pk>/',UserDetail.as_view()),
+    path('projectview/<int:pk>/',ProjectsDetail.as_view()),
+    path('TimeEntiryView/<int:pk>/',TimeEntiryDetail.as_view()),            
+]    
