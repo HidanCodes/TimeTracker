d2a71893353aa667c8978824aa5422b85e0e4b28
HidanCodes <hidancodes@gmail.com>
2025-03-01 11:26:02 +0330

    adding requirements and local setting file

--- TimeTracker/settings.py
+++ TimeTracker/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+from .local_settings import secret
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7xw$ywgc%xe-0j+_afm8bow5)no00g^w+#tp&#vf9+tn-2km_r'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
