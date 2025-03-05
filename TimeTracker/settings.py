881a5785f20307bb0fbab0b23c6a26600117ee60
HidanCodes <hidancodes@gmail.com>
2025-03-06 01:18:09 +0330

    s

--- TimeTracker/settings.py
+++ TimeTracker/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
-from .local_settings import *
+# from .local_settings import *
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
