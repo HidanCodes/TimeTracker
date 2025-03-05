6a7f866ec853538583dcf726c86cfb9c2e8ebbe0
HidanCodes <hidancodes@gmail.com>
2025-03-06 01:17:11 +0330

    s

--- TimeTracker/settings.py
+++ TimeTracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret
+SECRET_KEY = 'django-insecure-7xw$ywgc%xe-0j+_afm8bow5)no00g^w+#tp&#vf9+tn-2km_r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
